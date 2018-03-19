de423da9ae02a4bd665f15a99a1952b39312468f
yuqiancha <ping_jw@126.com>
2018-03-19 14:19:46 +0800

    First
    Create On Lab

---
+++ WWT_FacTest/WWT_FacTest/User_Magnetic_Confirm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WWT_FacTest
+{
+    partial class User_Magnetic_Confirm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(40, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(619, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "请将测试车移到车位锁上方，测试到地磁抖动会自动退出---倒计时：";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(370, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 37);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "地磁异常";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(118, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "地磁正常";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // User_Magnetic_Confirm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 170);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "User_Magnetic_Confirm";
+            this.Text = "User_Magnetic_Confirm";
+            this.Load += new System.EventHandler(this.User_Magnetic_Confirm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ WWT_FacTest/WWT_FacTest/User_Magnetic_Confirm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WWT_FacTest
+{
+    public partial class User_Magnetic_Confirm : Form
+    {
+        private int waitSecond = 20;
+        public User_Magnetic_Confirm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Data.MagneticStatus = true;
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Data.MagneticStatus = false;
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (waitSecond < 1)
+            {
+                this.Close();
+            }
+            else
+            {
+                waitSecond--;
+                label1.Text = "请将测试车移到车位锁上方，测试到地磁抖动会自动退出---倒计时：" + waitSecond + "秒";
+
+                SerialFun.SendToPort(SerialFun.ComPortSend, "010420020004");//查询
+                Thread.Sleep(100);
+                if (Data.ReturnStr.Substring(18, 2) == "55")
+                {
+                    Data.MagneticStatus = true;
+                    waitSecond = 0;
+                }
+            }
+        }
+
+        private void User_Magnetic_Confirm_Load(object sender, EventArgs e)
+        {
+            Data.MagneticStatus = false;
+        }
+    }
+}
