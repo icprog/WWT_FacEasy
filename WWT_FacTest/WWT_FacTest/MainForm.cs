138b819edaf3140ab729d93cd397c10b463b4bc8
yuqiancha <ping_jw@126.com>
2018-03-20 20:46:33 +0800

    add save xlsx
    not success yet!

--- WWT_FacTest/WWT_FacTest/MainForm.cs
+++ WWT_FacTest/WWT_FacTest/MainForm.cs
@@ -585,5 +585,57 @@ namespace WWT_FacTest
                 this.splitContainer3.Panel1Collapsed = true;
             }
         }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            string saveFileName = "";
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = "xlsx";
+            saveDialog.Filter = "Excel文件|*.xlsx";
+            saveDialog.FileName = fileName;
+            saveDialog.ShowDialog();
+            saveFileName = saveDialog.FileName;
+            if (saveFileName.IndexOf(":") < 0) return; //被点了取消
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                MessageBox.Show("无法创建Excel对象，您的电脑可能未安装Excel");
+                return;
+            }
+            Microsoft.Office.Interop.Excel.Workbooks workbooks = xlApp.Workbooks;
+            Microsoft.Office.Interop.Excel.Workbook workbook =
+                        workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+            Microsoft.Office.Interop.Excel.Worksheet worksheet =
+                        (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];//取得sheet1 
+                                                                                         //写入标题             
+            for (int i = 0; i < dataGridView2.ColumnCount; i++)
+            { worksheet.Cells[1, i + 1] = dataGridView2.Columns[i].HeaderText; }
+            //写入数值
+            for (int r = 0; r < dataGridView2.Rows.Count; r++)
+            {
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                {
+                    worksheet.Cells[r + 2, i + 1] = dataGridView2.Rows[r].Cells[i].Value;
+                }
+                System.Windows.Forms.Application.DoEvents();
+            }
+            worksheet.Columns.EntireColumn.AutoFit();//列宽自适应
+            MessageBox.Show(fileName + "资料保存成功", "提示", MessageBoxButtons.OK);
+            if (saveFileName != "")
+            {
+                try
+                {
+                    workbook.Saved = true;
+                    workbook.SaveCopyAs(saveFileName);  //fileSaved = true;                 
+                }
+                catch (Exception ex)
+                {//fileSaved = false;                      
+                    MessageBox.Show("导出文件时出错,文件可能正被打开！\n" + ex.Message);
+                }
+            }
+            xlApp.Quit();
+            GC.Collect();//强行销毁           
+        }
     }
 }
